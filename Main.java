25f829d24a5b9c1ffb464fd63a78e88ebb921505
justwin1 <justynaeola@gmail.com>
2020-10-19 16:41:17 +0200

    Zadanie 4. Dodawanie lastName

--- Main.java
+++ Main.java
@@ -11,11 +11,21 @@ class PeopleStats {
 class Person {
     private final String firstName;
 
-    public Person(String firstName) {
+    private String lastName;
+
+
+
+    public Person(String firstName, String lastName) {
         this.firstName = firstName;
+	this.lastName = lastName;
     }
 
     public String getFirstName() {
         return firstName;
     }
+
+    public String getLastName() {
+	return lastName;
+    }
+
 }
